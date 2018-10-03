0d4223bef6986e8de17272f64a31dbea51c5bb74
Maik Medeiros Moreira <maik.moreira@mobile.local>
2018-10-03 17:30:50 -0300

    c#

---
+++ ByteBank/Recovered-out-03-2018-0330.ContaCorrente.cs
@@ -0,0 +1,9 @@
+﻿
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int conta;
+    public double saldo;
+
+}
---
+++ ByteBank/Recovered-out-03-2018-0330.Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente Conta1 = new ContaCorrente();
+
+            Conta1.titular = "Gabriela";
+            Conta1.agencia = 123;
+            Conta1.conta = 123456;
+            Conta1.saldo = 0;
+
+            ContaCorrente Conta2 = new ContaCorrente();
+
+            Conta2.titular = "Gabriela";
+            Conta2.agencia = 123;
+            Conta2.conta = 123456;
+            Conta2.saldo = 0;
+        }
+    }
+}
