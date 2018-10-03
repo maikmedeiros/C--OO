d27054e7c4b755da26c85b27daa89e8ed68a6ee7
Maik Medeiros Moreira <maik.moreira@mobile.local>
2018-10-03 18:27:17 -0300

    c#

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente Conta1 = new ContaCorrente();
+
+            Conta1.titular = "Maik Medeiros";
+            Conta1.agencia = 888;
+            Conta1.conta = 123456;
+            Conta1.saldo = 150;
+
+            Console.WriteLine("Nome: " + Conta1.titular);
+            Console.WriteLine("Agencia: " + Conta1.agencia);
+            Console.WriteLine("Conta Corrente: " + Conta1.conta);
+            Conta1.saldo += 200;
+            Console.WriteLine("Saldo: " + Conta1.saldo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Maik";
+            conta.saldo += 10;
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.agencia);
+            Console.WriteLine(conta.conta);
+            Console.WriteLine(conta.saldo);
+        }
+    }
+}
